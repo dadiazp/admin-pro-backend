beb00092706a59325d39c701dc4a48253055e914
Daniel <dadiazp.16@est.ucab.edu.ve>
2021-08-06 20:52:20 -0400

    SPA Control

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const { dbConnection } = require('./database/config');
 //Paquete de npm dotenv, requiere instalacion previa
 require('dotenv').config();
 
+const path = require('path');
+
 //Paquete de npm cors, requiere instalacion previa
 const cors = require('cors');
 
@@ -38,6 +40,13 @@ app.use('/api/hospitales', require('./routes/hospitales'));
 app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+//Lo ultimo - Esto se hace para decirle a node que cuando se despliegue, debe atender a las rutas del frontend de angular
+//Con esto se evita que al recargar en heroku de error de la ruta. Cuando recargue y el backend no reconozca la url que está
+//colocada en el navegador entonces pasará a esta parte y le indicará que debe buscar las rutas en index.html, que son las rutas de angular
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'))
+});
 
 
 //Levantar la app
